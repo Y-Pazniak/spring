503e5aaa2aea49a95bd0519230ef74cb80783d96
Uladzimir Ilchanka <yauhenij@gmail.com>
2025-01-09 16:03:37 +0300

    Initial commit

---
+++ src/main/java/com/example/test/TestSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class TestSpringApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(TestSpringApplication.class, args);
+    }
+}
